82976b178632637325b56bf8db0bea5cc785bdf6
boubaSambare <boubasambare@yahoo.it>
2019-11-27 01:41:34 +0100

    slide setting

--- src/Components/MoviesList.jsx
+++ src/Components/MoviesList.jsx
@@ -15,14 +15,14 @@ class MoviesList extends Component {
       infinite: false,
       speed: 500,
       slidesToShow: 6,
-      slidesToScroll: 6,
+      slidesToScroll: 1,
       initialSlide: 0,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: true,
             dots: true
           }
