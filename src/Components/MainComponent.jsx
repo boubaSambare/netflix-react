73b50c7b957967e982c1ee8b16172283fcdceafe
boubaSambare <boubasambare@yahoo.it>
2019-12-23 21:42:15 +0100

    back office

--- src/Components/MainComponent.jsx
+++ src/Components/MainComponent.jsx
@@ -3,6 +3,7 @@ import NavComponent from "./NavComponent";
 import { Container, Row, Col } from "reactstrap";
 import MoviePage from "./MoviePage";
 import HomePage from "./HomePage";
+import BackOffice from './backoffice/BackOffice'
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 class MainComponent extends Component {
@@ -16,6 +17,7 @@ class MainComponent extends Component {
           <NavComponent></NavComponent>
           <Container>
             <Route path="/" exact component={HomePage}/>
+            <Route path="admin" component= {}/>
             <Route path="/details/:movieId" component={MoviePage} />
            
           </Container>
---
+++ src/Components/backoffice/BackOffice.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BackOffice extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default BackOffice;
